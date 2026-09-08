cff04fdb06f8b2400d3e25b5b3dcbf184ab687fe
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-08 07:10:46 +0530

    Update MAX_CHANNELS to process more channels
    Increased the maximum number of channels processed from 30 to 500.

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -6,7 +6,7 @@ from urllib3.util.retry import Retry
 
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
-MAX_CHANNELS = 30  # Ab yeh 30 channels process karega
+MAX_CHANNELS = 500  # Ab yeh 30 channels process karega
 MAX_WORKERS = 30  # Workers utne hi rakhe hain taaki server par load balance rahe
 
 # Session with automatic retry strategy setup kiya hai
