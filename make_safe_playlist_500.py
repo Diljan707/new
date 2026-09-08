ff0e8709b91a2bc73dd9f7d11aad686769772109
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-08 16:05:51 +0530

    Change MAX_CHANNELS limit to 1206
    Updated MAX_CHANNELS to limit to 1206 channels.

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -6,7 +6,7 @@ from urllib3.util.retry import Retry
 
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
-MAX_CHANNELS = 1706 # Sirf 1706 channels ke liye set kiya gaya hai
+MAX_CHANNELS = 1206 # Sirf 1206 channels ke liye set kiya gaya hai
 MAX_WORKERS = 5   
 
 # Session with automatic retry strategy setup kiya hai
