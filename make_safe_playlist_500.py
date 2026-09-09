9954a13bb2a00b85c9de9e3cb2e6b98fd32987a7
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-09 06:45:45 +0530

    Decrease MAX_WORKERS for processing
    Reduced the number of workers for processing from 40 to 5.

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -7,7 +7,7 @@ from urllib3.util.retry import Retry
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
 MAX_CHANNELS = 1099  # 1300 channels ke liye set kiya gaya hai
-MAX_WORKERS = 40  # Fast processing ke liye 20 workers set kiye hain
+MAX_WORKERS = 5  # Fast processing ke liye 20 workers set kiye hain
 
 
 # Session with automatic retry strategy setup kiya hai
