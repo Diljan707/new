28f524681601789e8c76a64c318c861111f653f1
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-11 07:36:57 +0530

    Update make_safe_playlist_500.py

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -6,7 +6,7 @@ from urllib3.util.retry import Retry
 
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
-MAX_CHANNELS = 1300
+MAX_CHANNELS = 3000
 MAX_WORKERS = 30
 
 
