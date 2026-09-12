f1b1dae49126d381c58e0187c3779d7679301440
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-12 15:31:10 +0530

    Decrease MAX_CHANNELS from 1200 to 1100

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -6,7 +6,7 @@ from urllib3.util.retry import Retry
 
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
-MAX_CHANNELS = 1200
+MAX_CHANNELS = 1100
 MAX_WORKERS = 1000
 
 def get_robust_session():
