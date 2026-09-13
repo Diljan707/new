17d1f62eeaa08ea584d8bd42d8df1c2482115bc4
Diljan707 <manjindersinghkhehra238@gmail.com>
2026-09-13 18:24:35 +0530

    Reduce MAX_CHANNELS and MAX_WORKERS limits

--- make_safe_playlist_500.py
+++ make_safe_playlist_500.py
@@ -6,8 +6,8 @@ from urllib3.util.retry import Retry
 
 PLAYLIST_URL = "https://game.playindia.fun/Jtv/RiYlIZ/Playlist.m3u"
 HEADERS = {"User-Agent": "Denver1769"}
-MAX_CHANNELS = 1500
-MAX_WORKERS = 1000
+MAX_CHANNELS = 1200
+MAX_WORKERS = 100
 
 def get_robust_session():
     session = requests.Session()
